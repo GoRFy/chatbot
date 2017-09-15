eea9d10a283caf167b7ad6e78e4892043ccf355b
GoRFy <lambot.rom@gmail.com>
2017-09-15 17:00:55 +0200

    Le bot fait semblant d'ecrire quand l'utilisateur ecrit doheavywork

--- app.js
+++ app.js
@@ -19,22 +19,43 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+    //session.send("You said: %s", session.message.text);
 
-    bot.on('conversationUpdate', function(message) {
+    /*bot.on('typing', function(){
+        session.send('You are writting');
+    });*/
+
+});
+
+bot.on('conversationUpdate', function(message) {
+    
+    if (message.membersAdded && message.membersAdded.length > 0) {
         
-        if (message.membersAdded && message.membersAdded.length > 0) {
-            
-            const isGroup = message.address.conversation.isGroup;
-            const txt = isGroup ? "Bienvenue inconnu!" : `Bienvenue ${message.membersAdded.map(curUser => curUser.name).toString()}`;
-            const reply = new builder.Message()
-                .address(message.address)
-                .text(txt);
-            bot.send(reply);
-        }  
-    });
+        const isGroup = message.address.conversation.isGroup;
+        const txt = isGroup ? "Bienvenue inconnu!" : `Bienvenue ${message.membersAdded.map(curUser => curUser.name).toString()}`;
+        const reply = new builder.Message()
+            .address(message.address)
+            .text(txt);
+        bot.send(reply);
+    }  
+});
 
-
-
+bot.dialog('doheavywork', function (session) {
+    session.sendTyping();
+    var intervalId = setInterval(
+        function () {
+            session.sendTyping();
+        },
+        1000
+    );
+    setTimeout(
+        function () {
+            session.send("Fin du travail");
+            clearInterval(intervalId);
+        },
+        2500
+    );
+}).triggerAction({
+    matches:  /^doheavywork$/,
 });
 
